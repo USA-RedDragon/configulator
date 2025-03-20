b56d0c1d16d880d4ca31eac32bb0bb04bac7d834
Jacob McSwain <jacob@mcswain.dev>
2025-03-19 21:44:17 -0500

    configulator: test envs

--- configulator_test.go
+++ configulator_test.go
@@ -215,3 +215,158 @@ func TestConfigulatorDefaults(t *testing.T) {
 	}
 
 }
+
+func TestConfigulatorEnvironmentVariables(t *testing.T) {
+	t.Setenv("TEST_BOOL", "false")
+	t.Setenv("TEST_INT", "20")
+	t.Setenv("TEST_INT8", "21")
+	t.Setenv("TEST_INT16", "22")
+	t.Setenv("TEST_INT32", "23")
+	t.Setenv("TEST_INT64", "24")
+	t.Setenv("TEST_UINT", "25")
+	t.Setenv("TEST_UINT8", "26")
+	t.Setenv("TEST_UINT16", "27")
+	t.Setenv("TEST_UINT32", "28")
+	t.Setenv("TEST_UINT64", "29")
+	t.Setenv("TEST_FLOAT32", "30.0")
+	t.Setenv("TEST_FLOAT64", "31.0")
+	t.Setenv("TEST_STRING", "32.0")
+	t.Setenv("TEST_SUBTESTCONFIG_BOOL", "false")
+	t.Setenv("TEST_SUBTESTCONFIG_INT", "40")
+	t.Setenv("TEST_SUBTESTCONFIG_INT8", "41")
+	t.Setenv("TEST_SUBTESTCONFIG_INT16", "42")
+	t.Setenv("TEST_SUBTESTCONFIG_INT32", "43")
+	t.Setenv("TEST_SUBTESTCONFIG_INT64", "44")
+	t.Setenv("TEST_SUBTESTCONFIG_UINT", "45")
+	t.Setenv("TEST_SUBTESTCONFIG_UINT8", "46")
+	t.Setenv("TEST_SUBTESTCONFIG_UINT16", "47")
+	t.Setenv("TEST_SUBTESTCONFIG_UINT32", "48")
+	t.Setenv("TEST_SUBTESTCONFIG_UINT64", "49")
+	t.Setenv("TEST_SUBTESTCONFIG_FLOAT32", "50.0")
+	t.Setenv("TEST_SUBTESTCONFIG_FLOAT64", "51.0")
+	t.Setenv("TEST_SUBTESTCONFIG_STRING", "52.0")
+
+	c := New[testConfig]()
+	c.WithEnvironmentVariables(&EnvironmentVariableOptions{
+		Prefix:    "TEST_",
+		Separator: "_",
+	})
+
+	cfg, err := c.Load()
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+
+	if cfg.Bool != false {
+		t.Fatalf("expected Bool to be false, got %v", cfg.Bool)
+	}
+
+	if cfg.Int != 20 {
+		t.Fatalf("expected Int to be 20, got %v", cfg.Int)
+	}
+
+	if cfg.Int8 != 21 {
+		t.Fatalf("expected Int8 to be 21, got %v", cfg.Int8)
+	}
+
+	if cfg.Int16 != 22 {
+		t.Fatalf("expected Int16 to be 22, got %v", cfg.Int16)
+	}
+
+	if cfg.Int32 != 23 {
+		t.Fatalf("expected Int32 to be 23, got %v", cfg.Int32)
+	}
+
+	if cfg.Int64 != 24 {
+		t.Fatalf("expected Int64 to be 24, got %v", cfg.Int64)
+	}
+
+	if cfg.Uint != 25 {
+		t.Fatalf("expected Uint to be 25, got %v", cfg.Uint)
+	}
+
+	if cfg.Uint8 != 26 {
+		t.Fatalf("expected Uint8 to be 26, got %v", cfg.Uint8)
+	}
+
+	if cfg.Uint16 != 27 {
+		t.Fatalf("expected Uint16 to be 27, got %v", cfg.Uint16)
+	}
+
+	if cfg.Uint32 != 28 {
+		t.Fatalf("expected Uint32 to be 28, got %v", cfg.Uint32)
+	}
+
+	if cfg.Uint64 != 29 {
+		t.Fatalf("expected Uint64 to be 29, got %v", cfg.Uint64)
+	}
+
+	if cfg.Float32 != 30.0 {
+		t.Fatalf("expected Float32 to be 30.0, got %v", cfg.Float32)
+	}
+
+	if cfg.Float64 != 31.0 {
+		t.Fatalf("expected Float64 to be 31.0, got %v", cfg.Float64)
+	}
+
+	if cfg.String != "32.0" {
+		t.Fatalf("expected String to be '32.0', got '%s'", cfg.String)
+	}
+
+	if cfg.SubTestConfig.Bool != false {
+		t.Fatalf("expected SubTestConfig.Bool to be false, got %v", cfg.SubTestConfig.Bool)
+	}
+
+	if cfg.SubTestConfig.Int != 40 {
+		t.Fatalf("expected SubTestConfig.Int to be 40, got %v", cfg.SubTestConfig.Int)
+	}
+
+	if cfg.SubTestConfig.Int8 != 41 {
+		t.Fatalf("expected SubTestConfig.Int8 to be 41, got %v", cfg.SubTestConfig.Int8)
+	}
+
+	if cfg.SubTestConfig.Int16 != 42 {
+		t.Fatalf("expected SubTestConfig.Int16 to be 42, got %v", cfg.SubTestConfig.Int16)
+	}
+
+	if cfg.SubTestConfig.Int32 != 43 {
+		t.Fatalf("expected SubTestConfig.Int32 to be 43, got %v", cfg.SubTestConfig.Int32)
+	}
+
+	if cfg.SubTestConfig.Int64 != 44 {
+		t.Fatalf("expected SubTestConfig.Int64 to be 44, got %v", cfg.SubTestConfig.Int64)
+	}
+
+	if cfg.SubTestConfig.Uint != 45 {
+		t.Fatalf("expected SubTestConfig.Uint to be 45, got %v", cfg.SubTestConfig.Uint)
+	}
+
+	if cfg.SubTestConfig.Uint8 != 46 {
+		t.Fatalf("expected SubTestConfig.Uint8 to be 46, got %v", cfg.SubTestConfig.Uint8)
+	}
+
+	if cfg.SubTestConfig.Uint16 != 47 {
+		t.Fatalf("expected SubTestConfig.Uint16 to be 47, got %v", cfg.SubTestConfig.Uint16)
+	}
+
+	if cfg.SubTestConfig.Uint32 != 48 {
+		t.Fatalf("expected SubTestConfig.Uint32 to be 48, got %v", cfg.SubTestConfig.Uint32)
+	}
+
+	if cfg.SubTestConfig.Uint64 != 49 {
+		t.Fatalf("expected SubTestConfig.Uint64 to be 49, got %v", cfg.SubTestConfig.Uint64)
+	}
+
+	if cfg.SubTestConfig.Float32 != 50.0 {
+		t.Fatalf("expected SubTestConfig.Float32 to be 50.0, got %v", cfg.SubTestConfig.Float32)
+	}
+
+	if cfg.SubTestConfig.Float64 != 51.0 {
+		t.Fatalf("expected SubTestConfig.Float64 to be 51.0, got %v", cfg.SubTestConfig.Float64)
+	}
+
+	if cfg.SubTestConfig.String != "52.0" {
+		t.Fatalf("expected SubTestConfig.String to be '52.0', got '%s'", cfg.SubTestConfig.String)
+	}
+
+}

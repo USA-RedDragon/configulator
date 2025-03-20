8ebf8216306c2c9961bf66a1fac6d72be21915f8
Jacob McSwain <jacob@mcswain.dev>
2025-03-19 21:13:01 -0500

    configulator: add tests for options

--- configulator_test.go
+++ configulator_test.go
@@ -1,10 +1,71 @@
-package configulator_test
+package configulator
 
 import (
 	"testing"
+
+	"github.com/spf13/pflag"
 )
 
-func TestConfigulator(t *testing.T) {
+type testConfig struct {
+	Bool bool `config:"bool"`
+}
+
+func (c testConfig) Validate() error {
+	return nil
+}
+
+func TestConfigulatorOptions(t *testing.T) {
 	t.Parallel()
-	// Test code here
+
+	pflags := pflag.NewFlagSet("test", pflag.ContinueOnError)
+
+	c := New[testConfig]()
+	if c == nil {
+		t.Fatal("expected non-nil Configulator")
+	}
+	c.WithPFlags(pflags, &PFlagOptions{
+		Separator: "-",
+	})
+	if c.pflagOptions == nil {
+		t.Fatal("expected non-nil PFlagOptions")
+	}
+	if c.pflagOptions.Separator != "-" {
+		t.Fatalf("expected separator '-', got '%s'", c.pflagOptions.Separator)
+	}
+	if c.flags != pflags {
+		t.Fatal("expected PFlags to be set")
+	}
+
+	c.WithArraySeparator(";")
+	if c.arraySeparator != ";" {
+		t.Fatalf("expected array separator ';', got '%s'", c.arraySeparator)
+	}
+
+	c.WithEnvironmentVariables(&EnvironmentVariableOptions{
+		Prefix:    "TEST_",
+		Separator: "_",
+	})
+	if c.envOptions == nil {
+		t.Fatal("expected non-nil EnvironmentVariableOptions")
+	}
+	if c.envOptions.Prefix != "TEST_" {
+		t.Fatalf("expected prefix 'TEST_', got '%s'", c.envOptions.Prefix)
+	}
+	if c.envOptions.Separator != "_" {
+		t.Fatalf("expected separator '_', got '%s'", c.envOptions.Separator)
+	}
+
+	c.WithFile(&FileOptions{
+		Paths: []string{"test.yaml"},
+	})
+	if c.fileOptions == nil {
+		t.Fatal("expected non-nil FileOptions")
+	}
+	if len(c.fileOptions.Paths) != 1 {
+		t.Fatal("expected one file path")
+	}
+	if c.fileOptions.Paths[0] != "test.yaml" {
+		t.Fatalf("expected file path 'test.yaml', got '%s'", c.fileOptions.Paths[0])
+	}
+
 }
